dff1b2b320b7da8bd1a8b544b9b40b27baeb9156
Archit Pandey <architpandeynitk@gmail.com>
2018-02-21 09:41:17 +0530

    Object oriented implementation for Craigslist bot added, README and final zip file

---
+++ craigslist/README.txt
@@ -0,0 +1,28 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++                                                                         +
++                                                                         +
++                   Craigslist Lost and Found Scraper                     +
++                                                                         +
++                                                                         +
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Pre-requisites:
+1. Python 2.7 (make sure python is in your path)
+2. Firefox and Geckodriver. (copy Geckodriver into C;/Python27)
+3. Selenium for Python
+4. bs4 for python
+
+How to run?
+1. Install all the pre-requisites.
+2. Double click scrape.py to start the scraping.
+
+Hey innovator1!
+
+I've created a program to do the tasks you desired. Apologies for the list of pre-requisites being quite long. I can install all these for you over a TeamViewer session if needed.
+
+In case you face any issues or need modifications to the functionality please let me know. I'll be more than happy to help you with that!
+
+Thanks again for your order! Looking forward to working with you again.
+
+Regards
+Archit.
--- craigslist/scrape.py
+++ craigslist/scrape.py
@@ -3,69 +3,119 @@ import bs4
 from Queue import Queue
 import time
 
-url = 'https://geo.craigslist.org/iso/us'
-Q = Queue()
-driver = webdriver.Firefox()
-driver.get(url)
+class scraper():
+    #init function
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.city_Q = Queue()
+        self.filename = "output.csv"
 
-page_html = driver.execute_script("return document.body.innerHTML")
-soup = bs4.BeautifulSoup(page_html, 'lxml')
-city_list = soup.find("ul", class_="geo-site-list")
-city_list = city_list.find_all("a")
-cities = list()
+    #function to get list of cities
+    def get_city_details(self):
+        url = 'https://geo.craigslist.org/iso/us'
+        self.driver.get(url)
+        page_html = self.driver.execute_script("return document.body.innerHTML")
+        soup = bs4.BeautifulSoup(page_html, 'lxml')
+        city_list = soup.find("ul", class_="geo-site-list")
+        city_list = city_list.find_all("a")
+        cities = list()
+        li = ''
+        for li in city_list:
+            city = {'name':'','link':''}
+            city['name'] = li.getText()
+            city['link'] = li['href']
+            cities.append(city)
+        return cities
 
-li = ''
+    #helper funciton to write a line to file
+    def write_line_to_file(self, line=None):
+        fp = open(self.filename, "a+")
+        content = fp.read()
+        fp.close()
+        if line in content:
+            print("Duplicate details " + line + ". Not printing to file!")
+        else:
+            print("Writing " + line + "to file...")
+            fp = open(self.filename, "a+")
+            fp.write(line + "\n")
+            fp.close()
 
-for li in city_list:
-    city = {'name':'','link':''}
-    city['name'] = li.getText()
-    city['link'] = li['href']
-    cities.append(city)
+    #function to write email and telephone number to file
+    def write_details_to_file(self, tel=None, email=None):
+        line = str(tel) + "," + str(email)
+        self.write_line_to_file(line)
 
-get_city = raw_input('Which city do you want to search?')
-if get_city == "all":
-    for city in cities:
-        Q.put(city)
-else:
-    for city in cities:
-        if get_city in city['name']:
-            Q.put(city)
+    #function to select the city based on user input
+    def select_city(self):
+        cities = self.get_city_details()
+        get_city = raw_input("Which city do you want to search? (Enter 'all' for all cities)\n---->")
+        if get_city == "all":
+            for city in cities:
+                self.city_Q.put(city)
+        else:
+            for city in cities:
+                if get_city in city['name']:
+                    self.city_Q.put(city)
 
-#get results from all cities in queue
-while not Q.empty():
-    city = Q.get()
-    lostfoundurl = city['link'] + '/search/laf'
-    driver.get(lostfoundurl)
-    totalcount = driver.find_element_by_class_name('totalcount').text
-    innerHTML = driver.execute_script("return document.body.innerHTML")
-    newsoup = bs4.BeautifulSoup(innerHTML, "lxml")
-    lost_list = newsoup.find("ul", class_="rows")
-    lost_list = lost_list.find_all("a")
-    if(len(lost_list) > int(totalcount)):
-        lost_list = lost_list[:int(totalcount)-1]
-    # add logic for pagination
-    page_urls = list()
-    for lost in lost_list:
-        if(lost['href'] != '#'):
-            page_urls.append(lost['href'])
+    #function to handle a single page and return the ad urls found
+    def handle_page(self, city):
+        print("Going to the city page")
+        lostfoundurl = city['link'] + '/search/laf'
+        self.driver.get(lostfoundurl)
+        totalcount = self.driver.find_element_by_class_name('totalcount').text
+        innerHTML = self.driver.execute_script("return document.body.innerHTML")
+        newsoup = bs4.BeautifulSoup(innerHTML, "lxml")
+        lost_list = newsoup.find("ul", class_="rows")
+        lost_list = lost_list.find_all("a", class_="result-title")
+        if(len(lost_list) > int(totalcount)):
+            lost_list = lost_list[:int(totalcount)-1]
+        elif(len(lost_list) < int(totalcount)):
+            while(len(lost_list) < int((totalcount))):
+                next_button = self.driver.find_element_by_class_name("next")
+                next_button.click()
+                innerHTML = self.driver.execute_script("return document.body.innerHTML")
+                newsoup = bs4.BeautifulSoup(innerHTML, "lxml")
+                new_lost_list = newsoup.find("ul", class_="rows")
+                new_lost_list = new_lost_list.find_all("a", class_="result-title")
+                lost_list += new_lost_list
 
-    for page_url in page_urls:
-        driver.get(page_url)
-        button = driver.find_element_by_class_name("reply_button")
-        button.click()
-        time.sleep(10)
-        try:
-            tel = driver.find_element_by_class_name("reply-tel-number").text
-            tel = tel.encode('utf8')
-            tel = tel[4:]
-            tel = tel.strip()
-        except:
-            tel = str(None)
-        try:
-            email = driver.find_element_by_class_name("mailapp").text
-            email = email.encode('utf8')
-            email = email.strip()
-        except:
-            email = None
+        page_urls = list()
+        for lost in lost_list:
+            if(lost['href'] != '#'):
+                page_urls.append(lost['href'])
 
-        print(tel + " " + email)
+        print("Found " + totalcount + "lost and found ad links")
+        return page_urls
+
+    #function to iterate through each page
+    def handle_pages(self):
+        self.select_city()
+        while not self.city_Q.empty():
+            city = self.city_Q.get()
+            page_urls = self.handle_page(city)
+            self.scrape(page_urls)
+
+    #function to get phone numbers from range of urls
+    def scrape(self,page_urls):
+        for page_url in page_urls:
+            self.driver.get(page_url)
+            button = self.driver.find_element_by_class_name("reply_button")
+            button.click()
+            time.sleep(10)
+            try:
+                tel = self.driver.find_element_by_class_name("reply-tel-number").text
+                tel = tel.encode('utf8')
+                tel = tel[4:]
+                tel = tel.strip()
+            except:
+                tel = str(None)
+            try:
+                email = self.driver.find_element_by_class_name("mailapp").text
+                email = email.encode('utf8')
+                email = email.strip()
+            except:
+                email = None
+            self.write_details_to_file(tel, email)
+
+s = scraper()
+s.handle_pages()
